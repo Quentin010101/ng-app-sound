b67f41b299fde9b644320f67679cd187d6370544
qcozic <q_cozic@hotmail.fr>
2024-06-14 17:50:14 +0200

    message

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -5,14 +5,29 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button'; 
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule,MatDividerModule],
+  imports: [CommonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule,MatDividerModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  public loginFormulaire!: FormGroup
 
+  constructor(){
+    this.loginFormulaire = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    })
+
+    console.log(this.loginFormulaire.invalid)
+  }
+
+  public onFormSubmit(){
+    console.log("submit")
+  }
 }
