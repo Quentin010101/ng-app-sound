3dd994b1187970a3c546737c6d39315f2c2fc35f
qcozic <q_cozic@hotmail.fr>
2024-06-14 14:05:08 +0200

    message

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card'; 
 import {MatInputModule} from '@angular/material/input'; 
 import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatInputModule, MatFormFieldModule],
+  imports: [CommonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule,MatDividerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
